46100a92a42b34eee48045fceda1708b213eb522
Yash Gopi Waghadhare <127091514+yashwaghadhare@users.noreply.github.com>
2023-05-29 14:37:07 +0530

    PDF-Splitter | Yash Waghadhare

---
+++ PDF_Splitter/index.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="PDF_Splitter.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF Splitter | yash-waghadhare</title>
+    <script src="BotFiles/bootstrap.bundle.min.js"></script>
+    <link href="BotFiles/bootstrap.min.css" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" />
+</head>
+<body class="bg-dark text-white">
+    <%--start of header--%>
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                        <use xlink:href="#bootstrap"></use></svg>
+                </a>
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+ <li><a href="index.aspx" class="nav-link px-2 text-white">Home</a></li>
+                    <li><a href="https://wa.me/message/Z6MB2AWXF7RCI1" class="nav-link px-2 text-white">Contact</a></li>
+                    <li><a href="https://yashwaghadhare.github.io/Personal_portfolio"
+                            class="nav-link px-2 text-white">Portfolio</a></li>
+                    <li><a href="https://yashwaghadhare.github.io/SnakkeGame/"
+                            class="nav-link px-2 text-white">PlayGame</a></li>
+                </ul>
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                </form>
+                <div class="text-end">
+                    <button type="button" class="btn btn-outline-light me-2">Login</button>
+                    <button type="button" class="btn btn-warning">Sign-up</button>
+                </div>
+            </div>
+        </div>
+        <hr class="text-white"/>
+    </header>
+    <%--end of header--%>
+
+
+
+    <%-- design part most dangeorus --%>
+    <div class="d-flex justify-content-center"">
+        <div class="book">
+            <%-- for frount change --%>
+            <form class=" cover-back d-flex justify-content-center" id="form2" runat="server">
+                <div>
+                    <asp:FileUpload class="form-control bg-dark text-white" type="file" ID="fileUpload" runat="server" /><br />
+                    <asp:TextBox Placeholder="Split range (e.g., 2-4)" ID="txtSplitRange" class="input text-white" name="text" type="text" runat="server"/><hr class="text-white"/>
+                     <asp:Button ID="btnSplit" class="btn btn-dark" runat="server" Text="Split" OnClick="btnSplit_Click" />
+                    <asp:Button ID="btnDownload" class="btn btn-dark"  runat="server" Text="Download" OnClick="btnDownload_Click" Visible="false" />
+                </div>
+            </form>
+
+            <%-- for frount --%>
+            <div class="cover d-flex justify-content-center">                
+                <img src="img/book.gif"/>
+            </div>
+            
+                
+        </div>
+    </div>
+
+    <%-- end design part most dangeorus --%>
+
+</body>
+</html>
---
+++ PDF_Splitter/index.aspx.cs
@@ -0,0 +1,144 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PDF_Splitter
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSplit_Click(object sender, EventArgs e)
+        {
+           if (fileUpload.HasFile)
+            {
+                try
+                {
+                    // Get the uploaded file
+                    HttpPostedFile uploadedFile = fileUpload.PostedFile;
+                    string fileName = Path.GetFileName(uploadedFile.FileName);
+                    string filePath = Server.MapPath("~/Uploads/") + fileName;
+
+                    // Save the uploaded file to the server
+                    uploadedFile.SaveAs(filePath);
+
+                    // Get the split range from the textbox
+                    string splitRange = txtSplitRange.Text.Trim();
+
+                    // Parse the split range input
+                    string[] rangeParts = splitRange.Split('-');
+                    if (rangeParts.Length != 2)
+                    {
+                        ShowPopup("Invalid split range format. Please enter a valid range (e.g., 2-4).");
+                        return;
+                    }
+
+                    int startPage, endPage;
+                    if (!int.TryParse(rangeParts[0], out startPage) || !int.TryParse(rangeParts[1], out endPage))
+                    {
+                        ShowPopup("Invalid split range format. Please enter a valid range (e.g., 2-4).");
+                        return;
+                    }
+
+                    // Create a reader to open the PDF
+                    PdfReader reader = new PdfReader(filePath);
+
+                    // Validate the split range
+                    int totalPages = reader.NumberOfPages;
+                    if (startPage < 1 || endPage > totalPages || startPage > endPage)
+                    {
+                        ShowPopup("Invalid split range. Please enter a valid range within the PDF page count.");
+                        reader.Close();
+                        return;
+                    }
+
+                    // Create a new document to store the combined pages
+                    Document document = new Document();
+                    string outputFileName = "SplittedFile.pdf";
+                    string outputFolderPath = Server.MapPath("~/Output/");
+                    string outputFilePath = Path.Combine(outputFolderPath, outputFileName);
+
+                    // Delete the file if it already exists
+                    if (File.Exists(outputFilePath))
+                    {
+                        File.Delete(outputFilePath);
+                    }
+
+                    PdfCopy copy = new PdfCopy(document, new FileStream(outputFilePath, FileMode.Create));
+                    document.Open();
+
+                    // Copy the specified range of pages to the new document
+                    for (int i = startPage; i <= endPage; i++)
+                    {
+                        copy.AddPage(copy.GetImportedPage(reader, i));
+                    }
+
+                    // Close the new document
+                    document.Close();
+
+                    // Close the PDF reader
+                    reader.Close();
+
+                    // Show the download button
+                    btnDownload.Visible = true;
+                    ViewState["OutputFilePath"] = outputFilePath; // Store the file path in ViewState
+                    ShowPopup("PDF Split successful!");
+                }
+                catch (Exception ex)
+                {
+                    ShowPopup("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                ShowPopup("Please select a file to upload.");
+            }
+        }
+
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (ViewState["OutputFilePath"] != null)
+            {
+                string outputFilePath = ViewState["OutputFilePath"].ToString();
+
+                if (File.Exists(outputFilePath))
+                {
+                    // Prepare the file download
+                    FileInfo file = new FileInfo(outputFilePath);
+                    Response.Clear();
+                    Response.ClearHeaders();
+                    Response.ClearContent();
+                    Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+                    Response.AddHeader("Content-Length", file.Length.ToString());
+                    Response.ContentType = "application/pdf";
+                    Response.Flush();
+
+                    // Write the file content directly to the response
+                    Response.TransmitFile(outputFilePath);
+
+                    // Delete the file from the server after download
+                    //File.Delete(outputFilePath);
+
+                    Response.End();
+                }
+
+                // Clear the ViewState
+                ViewState["OutputFilePath"] = null;
+            }
+        }
+
+        private void ShowPopup(string message)
+        {
+            ClientScript.RegisterStartupScript(GetType(), "Popup", "alert('" + message + "');", true);
+        }
+    }
+}
